34e2786ee19e902ea511d9e9ef815695e94a0c6c
Alessio Pragliola <seth.pro@gmail.com>
2016-07-19 14:54:59 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+
+app.get('*', function(req,res) {
+    var ip = req.headers['x-forwarded-for'];
+    var lang = req.headers['accept-language'];
+    var os = req.headers['user-agent'].toString();
+    
+    res.end(JSON.stringify({'ip': ip, 'lang': lang.substr(0,lang.indexOf(',')), 'os': os.substr(os.indexOf('(')+1,os.indexOf(')')-os.indexOf('(')-1)}));
+});
+
+app.listen(port);
